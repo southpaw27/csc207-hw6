cbd1a7e96bfe1fae283354c2948d32036b02c111
Mira <m13hall@gmail.com>
2013-10-06 15:59:31 -0500

    added tests

---
+++ src/edu/grinnell/csc207/kelleyan/hw6/DutchNFlagTest.java
@@ -0,0 +1,18 @@
+package edu.grinnell.csc207.kelleyan.hw6;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DutchNFlagTest {
+
+	@Test
+	public void testDutchFlag() {
+		String[] bestCase = {"red", "red", "red", "red", "white", "white", "blue", "blue", "blue", "blue", "blue"};
+		String[] worstCase = {"blue", "blue", "blue", "blue", "blue", "red", "red", "red", "red", "white", "white"};
+		assertEquals("test best case", bestCase, DutchNFlag.dutchFlag(bestCase));
+		assertEquals("test worst case", bestCase, DutchNFlag.dutchFlag(worstCase));
+		
+		
+	}
+
+}
