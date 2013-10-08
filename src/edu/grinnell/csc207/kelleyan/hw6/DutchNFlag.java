906d0fc54f87a8f5e517527db45614af248b6df6
southpaw27 <awkelley27@gmail.com>
2013-10-08 15:57:52 -0500

    comments added, need more tests

--- src/edu/grinnell/csc207/kelleyan/hw6/DutchNFlag.java
+++ src/edu/grinnell/csc207/kelleyan/hw6/DutchNFlag.java
@@ -2,42 +2,83 @@ package edu.grinnell.csc207.kelleyan.hw6;
 
 import java.io.PrintWriter;
 
+/**
+ * 
+ * @author southpaw14, Andrew Kelley
+ * @author Mira Hall
+ * 
+ * DutchNFlag is a class that contains the method to solve
+ * the classic problem of the Dutch National Flag. In this
+ * problem, an array of colors containing "red", "white", 
+ * and "blue" is given and must be sorted into the pattern
+ * of red, white, then blue. This sort must also be
+ * accomplished over one iteration of the array.
+ */
 public class DutchNFlag {
 	
 	
 
-	/**
-	 * @param args
-	 */
 	
+	/**
+	 * dutchFlag takes a string array of non-sorted colors
+	 * and returns the same array sorted in the order of
+	 * red, white, then blue.
+	 * 
+	 * @pre the strings in the array must only be either "red",
+	 * "white", or "blue" in english and lower case.
+	 * 
+	 * @param colors, a string array of red, white, and blue
+	 * colors
+	 * 
+	 * @return the same array, colors, the is now sorted by 
+	 * color into the order of red, then white, then blue.
+	 */
 	public static String[] dutchFlag(String[] colors) {
-		int i = 0;
-		int redFlag = 0;
-		int blueFlag = colors.length - 1;
+		int i = 0; //the iterator
+		int redFlag = 0; /*keeps track of the location of the
+						 farthest to the right "red" */
+		int blueFlag = colors.length - 1;  /*keeps track of the
+					location of the farthest to the right "red" */
 		while (i < colors.length){
 			if(colors[i] == "white"){
-				i++;
+				i++; /* White colors can be skipped over as they
+				will be in the middle and all the other colors
+				will be placed around them */
 			} else if(colors[i] == "red" && i > redFlag){
 				swap(colors, redFlag, i);
 				redFlag++;
-				continue;
+				continue; /* Need to recheck what was switched 
+				into colors[i] */
 			} else if(colors[i] == "blue" && i < blueFlag){
 				swap(colors, blueFlag, i);
 				blueFlag--;
-				continue;
+				continue; /* Need to recheck what was switched 
+				into colors[i] */
 			} else if(i <= redFlag || i >= blueFlag) {
-				i++;
+				i++; /* In these cases, the colors are in their
+				correct places, so no swapping is needed */
 			}
-		}
-		return colors;
-	}
+		} //end while
+		return colors; //return original array, now sorted
+	} //end dutchFlag
 	
+	/**
+	 * Swap is a helper-procedure that switches two items
+	 * in an array, specifically a String array.
+	 * @param stringArray, a string array
+	 * @param indexOne, an index of an item needing to be swapped
+	 * @param indexTwo, an index of an item needing to be swapped
+	 */
 	public static void swap(String[] stringArray, int indexOne, int indexTwo){
 		String tmp = stringArray[indexOne];
 		stringArray[indexOne] = stringArray[indexTwo];
 		stringArray[indexTwo] = tmp;
-	}
+	} //end swap
 	
+	/**
+	 * main allows for a demonstration of the above algorithm.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		PrintWriter pen = new PrintWriter(System.out, true);
 		String[] unsortedFlag = new String[] {"blue", "red", "white", "red", "red", "white", "blue", "red", "blue", "white"};
@@ -45,7 +86,6 @@ public class DutchNFlag {
 		for(int i = 0; i < sortedFlag.length; i++){
 		pen.println(sortedFlag[i] + " ");
 		}
-		//pen.println("end");
-	}
+	} //end main
 
-}
+} //end DutchNFlag
--- src/edu/grinnell/csc207/kelleyan/hw6/DutchNFlagTest.java
+++ src/edu/grinnell/csc207/kelleyan/hw6/DutchNFlagTest.java
@@ -9,8 +9,8 @@ public class DutchNFlagTest {
 	public void testDutchFlag() {
 		String[] bestCase = {"red", "red", "red", "red", "white", "white", "blue", "blue", "blue", "blue", "blue"};
 		String[] worstCase = {"blue", "blue", "blue", "blue", "blue", "red", "red", "red", "red", "white", "white"};
-		assertEquals("test best case", bestCase, DutchNFlag.dutchFlag(bestCase));
-		assertEquals("test worst case", bestCase, DutchNFlag.dutchFlag(worstCase));
+		assertArrayEquals("test best case", bestCase, DutchNFlag.dutchFlag(bestCase));
+		assertArrayEquals("test worst case", bestCase, DutchNFlag.dutchFlag(worstCase));
 		
 		
 	}
